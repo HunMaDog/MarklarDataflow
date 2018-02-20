d1c8a81ea85616e4a2d650ec2ccd6d82d3822d2c
Istvan Simon <istvan.simon@gmail.com>
2018-02-20 14:51:03 +0100

    added google test build script and build list

---
+++ 3rdparty/build.linux.googletest.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Stop when error
+set -e
+
+# ###########################
+# Setup
+# ###########################
+
+# Name of the 3rd party project
+BUILDSCRIPT_PROJECT_NAME=GoogleTest
+BUILDSCRIPT_PROJECT_SUBDIR=googletest
+
+# Absolute path to this script
+BUILDSCRIPT_SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in
+BUILDSCRIPT_SCRIPT_PATH=$(dirname "${BUILDSCRIPT_SCRIPT}")
+
+# build type is default RelWithDebInfo
+BUILDSCRIPT_BUILD_TYPE="RelWithDebInfo"
+if [ "$1" != "" ]; then
+    BUILDSCRIPT_BUILD_TYPE="$1"
+fi
+
+# Main build directory
+BUILDSCRIPT_BUILD_PATH=${BUILDSCRIPT_SCRIPT_PATH}/build/${BUILDSCRIPT_PROJECT_SUBDIR}/${BUILDSCRIPT_BUILD_TYPE}/
+BUILDSCRIPT_PROJECT_PATH=${BUILDSCRIPT_SCRIPT_PATH}/${BUILDSCRIPT_PROJECT_SUBDIR}
+
+# Number of core
+BUILDSCRIPT_NUM_OF_CORE="$(nproc --all)"
+echo "Number of cores: ${BUILDSCRIPT_NUM_OF_CORE}"
+
+# ###########################
+# Preparing
+# ###########################
+
+# Remove old files
+echo "Removing old files"
+rm -rf  ${BUILDSCRIPT_BUILD_PATH}
+
+# create build dir
+echo "Create build directory"
+mkdir -p ${BUILDSCRIPT_BUILD_PATH}
+
+# ###########################
+# Build
+# ###########################
+
+echo "${BUILDSCRIPT_PROJECT_NAME} ${BUILDSCRIPT_BUILD_TYPE} building..."
+
+# building
+cd ${BUILDSCRIPT_BUILD_PATH}
+
+cmake ${BUILDSCRIPT_PROJECT_PATH} -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=${BUILDSCRIPT_BUILD_TYPE} 
+
+make -j${BUILDSCRIPT_NUM_OF_CORE} all
